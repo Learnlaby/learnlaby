2c97ce4cd2221184b874bbe7b35547b0a6d3118c
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2025-02-05 14:19:23 +0700

    Fix the sizing

--- components/ClassroomCard.tsx
+++ components/ClassroomCard.tsx
@@ -1,12 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const ClassroomCard = () => {
   return (
     <li className='classroom-card' aria-label="Classroom Card">
       <article>
         <div className="image-container">
-          <Image src="https://placehold.co/216x160" alt="Classroom Image" width={216} height={160} />
+          <Image 
+            src="https://placehold.co/216x160" 
+            alt="Classroom Image" 
+            width={216} // Set the width
+            height={160} // Set the height
+            layout="responsive" // This will maintain the aspect ratio
+          />
         </div>
         <div className="content">
           <h3>Classroom Name</h3>
@@ -14,7 +20,7 @@ const ClassroomCard = () => {
         </div>
       </article>
     </li>
-  )
+  );
 }
 
-export default ClassroomCard
+export default ClassroomCard;
