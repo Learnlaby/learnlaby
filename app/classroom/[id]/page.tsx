cf6af242299443c3dcdc5d98d6ede3a08b238c79
Thorung Boonkaew <thorung.b@ku.th>
2025-02-08 19:31:20 +0700

    Apply navbar and sidebar into classroom page

--- app/classroom/[id]/page.tsx
+++ app/classroom/[id]/page.tsx
@@ -1,9 +1,13 @@
 "use client";
-
 import { useParams } from "next/navigation";
+import Layout from "@/components/layout";
 
 export default function ClassroomPage() {
   const { classroomId } = useParams();
 
-  return <h1>Page {classroomId}</h1>;
+  return (
+    <Layout>
+      <h1>Classroom {classroomId}</h1>
+    </Layout>
+  );
 }
