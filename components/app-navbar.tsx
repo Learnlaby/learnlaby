f7be254bd7429797803dd1c946c73b40cd56ea77
Thorung Boonkaew <thorung.b@ku.th>
2025-02-08 18:29:41 +0700

    Apply sidebar trigger into navbar

--- components/app-navbar.tsx
+++ components/app-navbar.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import { FaPlus, FaBars } from "react-icons/fa";
+import { FaBars, FaPlus } from "react-icons/fa";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import styles from './index.module.css';
 
 const AppNavbar = () => {
     return (
-        <nav className="flex items-center justify-between p-1 bg-white shadow-md">
-            <div className="flex items-center gap-4">
-                <button className="p-2 rounded-full hover:bg-gray-200">
-                    <FaBars className={styles.fabarsIcon} />
-                </button>
+        <nav className="flex items-center justify-between p-2 bg-white shadow-md w-full text-sm fixed top-0 left-0 z-10">
+            <div className="flex items-center gap-2">
+                <SidebarTrigger>
+                    <button className="p-1 rounded-full hover:bg-gray-200">
+                        <FaBars className={styles.faplusIcon} />
+                    </button>
+                </SidebarTrigger>
                 <b className={styles.learnlabyLogo}>Learnlaby</b>
             </div>
-            <div className="flex items-center gap-2">
-                <button className="p-2 rounded-full hover:bg-gray-200">
+
+            <div className="flex items-center gap-1">
+                <button className="p-1 rounded-full hover:bg-gray-200">
                     <FaPlus className={styles.faplusIcon} />
                 </button>
-                <div className="w-10 h-10 rounded-full bg-purple-300"></div> {/* Profile Image Placeholder */}
+                <button className="p-1 rounded-full hover:bg-gray-200">
+                    <div className="w-8 h-8 rounded-full bg-purple-300"></div>
+                </button>
             </div>
         </nav>
     );
