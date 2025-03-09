8782d3b6a78e40ed1672eb6121da2e9cf90d7996
Chayakarn Hengsuwan <113860728+colarrbear@users.noreply.github.com>
2025-03-09 17:42:31 +0700

    deleted duplicate navbar, invite tab

--- app/classroom/[id]/invite/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function InvitePage({ params }) {
-  const { id } = params
-  // Redirect to the member page which now includes the invite functionality
-  redirect(`/classroom/${id}/member`)
-}
-
